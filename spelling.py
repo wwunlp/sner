098b971580879f11d15a6cacf88ab2b8e05a5301
QuakeIV <dwarfgaurd@gmail.com>
2016-05-02 20:49:21 -0700

    outlined spelling stuff

--- spelling.py
+++ spelling.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 
-# TODO: outline spelling parsing
+
+#map of 1 2 and 3 grams matched to the number of occurences of that n-gram
+
+
+
+#utility functions to insert grams into maps
+
+
+
+#xml parser to fetch names from the xml file and harvest the grams
+
+
+
+#collect percentage statistics from the gram maps
+
