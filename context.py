562f9940c25dc5df3d5a8ea9fdc3e79fd5289897
Mike Canoy <canoym@wwu.edu>
2016-09-23 14:36:33 -0700

    Using editdistance in context.

--- context.py
+++ context.py
@@ -4,26 +4,31 @@
 # Authored     5/3/2016
 # Last updated 5/9/2016
 
+import editdistance
 
 left_rules = {}
 right_rules = {}
 skipped = {'lines' : 0}
 
 def main(text, name):
+    skip = True
     text = text.split(' ')
-    if name in text:
-        index = text.index(name)
-        if index - 1 >= 0:
-            left = text[index - 1]
-            if left in left_rules.keys():
-                left_rules[left] += 1
-            else:
-                left_rules[left] = 1
-        if index + 1 <= len(text) - 1:
-            right = text[index + 1]
-            if right in right_rules.keys():
-                right_rules[right] += 1
-            else:
-                right_rules[right] = 1
-    else:
+    for word in text:
+        if editdistance.eval(name, word) < 2:
+            skip = False
+            index = text.index(word)
+            if index - 1 >= 0:
+                left = text[index - 1]
+                if left in left_rules.keys():
+                    left_rules[left] += 1
+                else:
+                    left_rules[left] = 1
+            if index + 1 <= len(text) - 1:
+                right = text[index + 1]
+                if right in right_rules.keys():
+                    right_rules[right] += 1
+                else:
+                    right_rules[right] = 1
+    
+    if skip:
         skipped['lines'] += 1
