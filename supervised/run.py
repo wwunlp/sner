de5bfd1aec2a4f751ee9986fff9a1acf28bb0aac
terryl <terryl@students.wwu.edu>
2016-10-26 16:08:43 -0700

    All supervised and things run from init.py should now be PEP8 compliant.

--- supervised/run.py
+++ supervised/run.py
@@ -4,34 +4,38 @@ import namesfromrules
 import contextualfromnames
 import spellingfromnames
 
-#reads the corpus file into memory by prompting the user for a file name
-#returns the contents of the corpus as a TokenSet
-#kindof a horribly bloody mess right now
+# Reads the corpus file into memory by prompting the user for a file name.
+# Returns the contents of the corpus as a TokenSet.
+# Kind of a horribly bloody mess right now.
+
+
 def loadCorpus():
+
     rawcorpus = list()
 
-    #prompt for the name of the corpus file
+    # Prompt for the name of the corpus file.
     corpusname = input("please enter the name of the corpus data file: ")
 
     try:
         corpusfile = open(corpusname, "r")
     except:
         print("ERROR: Unable to open file: " + corpusname)
-        
-    #skip the first line since its column identifiers
+
+    # Skip the first line since its column identifiers
     next(corpusfile)
-    #read the corpus from the user-specified file
+    # Read the corpus from the user-specified file
     for line in corpusfile:
-        #clean out the line returns as they will confuse the algorithm
-        line = line.replace('\n','')
+        # Clean out the line returns as they will confuse the algorithm
+        line = line.replace('\n', '')
         rawcorpus.append(line.split(','))
-        
+
     corpusfile.close()
-    
+
     corpus = TokenSet()
-    
-    #convert the line into the appropriate format
-    #each entry being a three element list, containing left context, token, right context
+
+    # Convert the line into the appropriate format
+    # Each entry being a three element list, containing left context, token,
+    #  right context
 
     def interpretTokenType(wordType):
         if wordType == "PN":
@@ -40,29 +44,33 @@ def loadCorpus():
             return TokenType.none
         else:
             tb = sys.exc_info()[2]
-            raise TypeError("Unrecognized Word Type in corpus file!").with_traceback(tb)
-            
+            raise TypeError("Unrecognized Word Type in corpus" +
+                            " file!").with_traceback(tb)
 
-    #here you see loop unrolling
+    # Here you see loop unrolling
     rawIter = iter(rawcorpus)
     line = next(rawIter)
     tabletID = line[0]
-    #inputs here are, the left context for the token, the token itself, the right context, and then the type of the token (for instance if its annotated as a PN)
-    #right context is assumed to be nothing, and will be set later if otherwise
+    # Inputs here are: the left context for the token, the token itself,
+    # the right context, and then the type of the token
+    #  (for instance if its annotated as a PN)
+    # right context is assumed to be nothing,
+    # and will be set later if otherwise
     prevToken = Token(None, line[3], None, interpretTokenType(line[4]))
-    
+
     for line in rawIter:
         if(line[0] == tabletID):
-            token = Token(prevToken, line[3], None, interpretTokenType(line[4]))
-            
+            token = Token(prevToken, line[3], None,
+                          interpretTokenType(line[4]))
+
             prevToken.right_context = str(token)
             corpus.addToken(prevToken)
-    
+
             prevToken = token
-            
+
         else:
             corpus.addToken(prevToken)
-            
+
             tabletID = line[0]
             token = Token(None, line[3], None, interpretTokenType(line[4]))
             prevToken = token
@@ -71,26 +79,26 @@ def loadCorpus():
 
     return corpus
 
-#this function will read the seed rule file, and return the contents as a RuleSet (defined in ruleset.py)
+
+# This function will read the seed rule file, and return the contents as
+# a RuleSet (defined in ruleset.py)
 def loadSeedRules():
     rules = RuleSet()
 
-    #prompt for the name of the seed rule file 
+    # Prompt for the name of the seed rule file
     rulename = input("please enter the name of the seed rule file: ")
-    
+
     try:
         rulefile = open(rulename, "r")
     except:
         print("ERROR: Unable to open file: " + rulename)
 
-
-    #read the corpus from the user-specified file
-
-    #skip the first line since its column identifiers
+    # Read the corpus from the user-specified file.
+    # Skip the first line since its column identifiers.
     next(rulefile)
     for line in rulefile:
-        #clean out the line returns as they will confuse the algorithm
-        line = line.replace('\n','')
+        # Clean out the line returns as they will confuse the algorithm.
+        line = line.replace('\n', '')
         ruledata = line.split(",")
         ruletype = RuleType.unset
 
@@ -108,88 +116,98 @@ def loadSeedRules():
 
     return rules
 
-#orchestrate the execution of the program
+
+# Orchestrate the execution of the program
 def main():
-    #rules and names will be lists of RuleSets or TokenSets
-    #these sets will represent the results of various iterations of the algorithm
-    #so index 0 of rules would be the first rule set (seed rules) and 1 would be
-    #the first rules generated and used by the algorithm itself
-    #index zero of names would be the names that came from the seed rules
-    #index one the rules that came from rule set 1
-    #et cetera
+    # Rules and names will be lists of RuleSets or TokenSets.
+    # These sets will represent the results of various iterations of
+    # the algorithm. So index 0 of rules would be the first rule set
+    # (seed rules) and 1 would be the first rules generated and used by the
+    # algorithm itself.
+    # Index zero of names would be the names that came from the seed rules.
+    # Index one the rules that came from rule set 1.
+    # And so on.
     rulestack = list()
     names = list()
 
-    #meant to store all rules that are ultimately used by the algorithm
+    # Meant to store all rules that are ultimately used by the algorithm
     allrules = RuleSet()
 
-    #load and prepare corpus data
-    #corpus is a TokenSet, which is a container for many Token items
-    #Tokens represent individual words in the corpus, and contain left and right context as well as annotations
+    # Load and prepare corpus data.
+    # Corpus is a TokenSet, which is a container for many Token items.
+    # Tokens represent individual words in the corpus, and contain left
+    #  and right context as well as annotations
     corpus = loadCorpus()
-    
-    #get the seed rules
+
+    # Get the seed rules
     seedrules = loadSeedRules()
     rulestack.append(seedrules)
     allrules.extend(seedrules)
-        
-    #identify names via seed rules
+
+    # Identify names via seed rules
     newNames = namesfromrules.run(corpus, seedrules)
     names.append(newNames)
 
     contextualIteration = False
 
     iterations = int(input("please enter the number of iterations: "))
-    maxrules = int(input("please enter the number of rules to keep between iterations: "))
-    
-    #begin iterating
-    i = 0;
+    maxrules = int(input("please enter the number of rules to keep between" +
+                         " iterations: "))
+
+    # Begin iterating.
+    i = 0
     while i < iterations:
         if contextualIteration:
             print("iteration " + str(i + 1) + ": find contextual rules")
-            newRules = contextualfromnames.run(corpus, allrules, names[i], maxrules)
+            newRules = contextualfromnames.run(corpus, allrules, names[i],
+                                               maxrules)
             rulesFound = len(list(newRules.rules))
             print("found " + str(rulesFound) + " new contextual rules!")
 
             rulestack.append(newRules)
             allrules.extend(newRules)
 
-            #get the names for the next iteration
-            newNames = namesfromrules.run(corpus,newRules)
-            print("top " + str(rulesFound) + " rules found " + str(len(newNames.tokens)) + " new names")
+            # Get the names for the next iteration.
+            newNames = namesfromrules.run(corpus, newRules)
+            print("top " + str(rulesFound) + " rules found " +
+                  str(len(newNames.tokens)) + " new names")
             names.append(newNames)
         else:
             print("iteration " + str(i + 1) + ": spelling")
-            newRules = spellingfromnames.run(corpus, allrules, names[i], maxrules)
+            newRules = spellingfromnames.run(corpus, allrules, names[i],
+                                             maxrules)
             rulesFound = len(list(newRules.rules))
             print("found " + str(rulesFound) + " new spelling rules!")
 
             rulestack.append(newRules)
             allrules.extend(newRules)
-            
-            #get the names for the next iteration
-            newNames = namesfromrules.run(corpus,newRules)
-            print("top " + str(rulesFound) + " rules found " + str(len(newNames.tokens)) + " new names")
+
+            # Get the names for the next iteration
+            newNames = namesfromrules.run(corpus, newRules)
+            print("top " + str(rulesFound) + " rules found " +
+                  str(len(newNames.tokens)) + " new names")
             names.append(newNames)
 
         contextualIteration = not contextualIteration
         i += 1
 
-    #debug output
-    #i = 0
-    #for rule in allrules:
-    #    print("rule " + str(i) + ": " + str(rule.rtype) + " " + str(rule.contents) + " " + str(rule.strength))
+    # Debug output
+    # i = 0
+    # for rule in allrules:
+    #    print("rule " + str(i) + ": " + str(rule.rtype) + " " +
+    #          str(rule.contents) + " " + str(rule.strength))
     #    i += 1
 
-    #really half assed output system, needs upgrading
+    # Really half assed output system, needs upgrading.
 
     f = open("output.csv", 'wb')
-    
+
     f.write("Rule,Rule Type,Strength\n".encode('utf-8'))
     for rule in allrules:
-        f.write("{0},{1},{2}\n".format(str(rule.contents), str(rule.rtype), str(rule.strength)).encode('utf-8'))
+        f.write("{0},{1},{2}\n".format(str(rule.contents), str(rule.rtype),
+                str(rule.strength)).encode('utf-8'))
 
     f.close()
 
-#begin execution
+# Begin execution
 main()
--- supervised/tokenset.py
+++ supervised/tokenset.py
@@ -2,15 +2,18 @@
 from enum import Enum
 import sys
 
+
 class TokenType(Enum):
     unset = -1
     none = 0
     personal_name = 1
 
-#this represents a given 'token' in the corpus
-#it represents a combination of left context, right context, and the word itself
-#any identical tokens will be combined into pre-existing ones
-#this is meant to be used in conjunction with the TokenSet implemented below
+
+# This represents a given 'token' in the corpus.
+# It represents a combination of left context, right context, and the word
+#  itself.
+# Any identical tokens will be combined into pre-existing ones.
+# This is meant to be used in conjunction with the TokenSet implemented below.
 class Token:
     token = None
     annotation = TokenType.unset
@@ -19,16 +22,18 @@ class Token:
     occurrences = 1
 
     def key(self):
-        return ((self.token + str(self.left_context) + str(self.right_context)), self.annotation.value)
+        return ((self.token + str(self.left_context) +
+                str(self.right_context)), self.annotation.value)
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self.key() == other.key()
-        
+
         return False
-    
+
     def __hash__(self):
-        return hash(self.token + str(self.left_context) + str(self.right_context) + str(self.annotation.value))
+        return hash(self.token + str(self.left_context) +
+                    str(self.right_context) + str(self.annotation.value))
 
     def __init__(self, left, token, right, annotation):
         self.left_context = str(left)
@@ -39,9 +44,11 @@ class Token:
 
     def __str__(self):
         return self.token
-        
-#this acts as an effecient container for Token objects, take my word for it
-#it will enforce 'Set' style behavior on the tokens in that no two identical token objects will be allowed
+
+
+# This acts as an effecient container for Token objects, take my word for it.
+# It will enforce 'Set' style behavior on the tokens in that no two identical
+# token objects will be allowed.
 class TokenSet:
     tokens = dict()
 
@@ -51,14 +58,15 @@ class TokenSet:
     def addToken(self, token):
         if(type(token) is not Token):
             tb = sys.exc_info()[2]
-            raise TypeError("Tried to push something other than a token into a TokenSet").with_traceback(tb)
+            raise TypeError("Tried to push something other than a token" +
+                            " into a TokenSet").with_traceback(tb)
 
         try:
             existingtoken = self.tokens[token]
         except KeyError:
             existingtoken = None
-        
-        if(existingtoken == None):
+
+        if(existingtoken is None):
             self.tokens[token] = token
         else:
             existingtoken.occurrences += 1
@@ -66,8 +74,10 @@ class TokenSet:
     def extend(self, setOfTokens):
         if(type(setOfTokens) is not TokenSet):
             tb = sys.exc_info()[2]
-            raise TypeError("Attempted to add contents of TokenSet to another TokenSet, was given something other than a TokenSet to add").with_traceback(tb)
-        
+            raise TypeError("Attempted to add contents of TokenSet to" +
+                            " another TokenSet, was given something other" +
+                            " than a TokenSet to add").with_traceback(tb)
+
         for token in setOfTokens:
             self.addToken(token)
 
