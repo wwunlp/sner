4d5574bccc224ede25727ce4d8f5903fbf88976d
Mike Canoy <canoym@wwu.edu>
2017-01-27 11:26:50 -0800

    Fix model files.

--- models/decision.py
+++ models/decision.py
@@ -1,178 +1,139 @@
-<!DOCTYPE html>
-<html lang='en'>
-<head prefix='og: http://ogp.me/ns#'>
-<meta charset='utf-8'>
-<meta content='IE=edge' http-equiv='X-UA-Compatible'>
-<meta content='object' property='og:type'>
-<meta content='GitLab' property='og:site_name'>
-<meta content='Sign in' property='og:title'>
-<meta content='WWU CSCI GitLab' property='og:description'>
-<meta content='https://gitlab.cs.wwu.edu/assets/gitlab_logo-cdf021b35c4e6bb149e26460f26fae81e80552bc879179dd80e9e9266b14e894.png' property='og:image'>
-<meta content='https://gitlab.cs.wwu.edu/users/sign_in' property='og:url'>
-<meta content='summary' property='twitter:card'>
-<meta content='Sign in' property='twitter:title'>
-<meta content='WWU CSCI GitLab' property='twitter:description'>
-<meta content='https://gitlab.cs.wwu.edu/assets/gitlab_logo-cdf021b35c4e6bb149e26460f26fae81e80552bc879179dd80e9e9266b14e894.png' property='twitter:image'>
+from scipy.sparse import coo_matrix
+import numpy
+from sklearn import datasets
+from sklearn import tree
+from sklearn.metrics import mean_squared_error, median_absolute_error, accuracy_score, recall_score, precision_score
+import random
+import os.path
 
-<title>Sign in · GitLab</title>
-<meta content='WWU CSCI GitLab' name='description'>
-<link rel="shortcut icon" type="image/x-icon" href="/assets/favicon-075eba76312e8421991a0c1f89a89ee81678bcde72319dd3e8047e2a47cd3a42.ico" />
-<link rel="stylesheet" media="all" href="/assets/application-6867d81d1da4ea60f24c4e4f0335d21126326e94418c9de357cf3c397926ee03.css" />
-<link rel="stylesheet" media="print" href="/assets/print-68eed6d8135d858318821e790e25da27b2b4b9b8dbb1993fa6765d8e2e3e16ee.css" />
-<script src="/assets/application-95adfce412d4f6bc8ed3f7894a4e1572f04202ad6090f05b74ccf98757f61889.js"></script>
-<meta name="csrf-param" content="authenticity_token" />
-<meta name="csrf-token" content="s9O5+o0q2UGVF9CxcT5OoMu1wV1LM2hf2GWK9w4KBAOcmpHKw+8dpBzIH8WLOVBXa7no1ade2dunmtcXOtxPtw==" />
-<script>
-//<![CDATA[
-window.gon={};gon.api_version="v3";gon.default_avatar_url="https://gitlab.cs.wwu.edu/assets/no_avatar-07eeb128b993e74003e8664cff0b8e1e7234cec0443766a6763df32ca3472c23.png";gon.default_issues_tracker="gitlab";gon.max_file_size=10;gon.relative_url_root="";gon.shortcuts_path="/help/shortcuts";gon.user_color_scheme="white";
-//]]>
-</script>
-<meta content='origin-when-cross-origin' name='referrer'>
-<meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'>
-<meta content='#474D57' name='theme-color'>
-<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-iphone-2d64ecc33893baab71adc15ff19a803a59911cc2651fb9d4d62af1379ff89aab.png" />
-<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-ipad-d08897d57e1bc9400024ac15465340e832a8e7b166b91624138d48ea2c739596.png" sizes="76x76" />
-<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-iphone-retina-81446c57f3351d1dacd0fb5f23ced74ba63d3878810bedea343999c6a12b3915.png" sizes="120x120" />
-<link rel="apple-touch-icon" type="image/x-icon" href="/assets/touch-icon-ipad-retina-e2a776da039936ac240e76615add47b25ab77c75a5fa2d1b3907f83d5502b911.png" sizes="152x152" />
-<link color='rgb(226, 67, 41)' href='/assets/logo-d36b5212042cebc89b96df4bf6ac24e43db316143e89926c0db839ff694d2de4.svg' rel='mask-icon'>
-<meta content='/assets/msapplication-tile-49c9c46afd2ab9bbf35e69138bc62f8c31fa55584bd494956ac6e58e6aadc813.png' name='msapplication-TileImage'>
-<meta content='#30353E' name='msapplication-TileColor'>
+print("Reading data")
+
+
+feature_count = sum(1 for line in open("data/features.KEY", "r"))
 
 
 
+Y = numpy.loadtxt("data/target_train.RT")
+A = numpy.loadtxt("data/features_train.sparseX", ndmin=2)
+I = A[:, 0]
+J = A[:, 1]
+data = A[:, 2]
+X = coo_matrix((data, (I, J)), shape=(int(max(I))+1, feature_count))
 
-<style>
-  [data-user-is] {
-    display: none !important;
-  }
-  
-  [data-user-is=""] {
-    display: block !important;
-  }
-  
-  [data-user-is=""][data-display="inline"] {
-    display: inline !important;
-  }
-  
-  [data-user-is-not] {
-    display: block !important;
-  }
-  
-  [data-user-is-not][data-display="inline"] {
-    display: inline !important;
-  }
-  
-  [data-user-is-not=""] {
-    display: none !important;
-  }
-</style>
+print (X.shape)
 
-</head>
-
-<body class='ui_charcoal login-page application navless'>
-<header class='navbar navbar-fixed-top navbar-empty'>
-<div class='container'>
-<div class='center-logo'>
-<svg width="36" height="36" id="tanuki-logo">
-  <path id="tanuki-right-ear" class="tanuki-shape" fill="#e24329" d="M2 14l9.38 9v-9l-4-12.28c-.205-.632-1.176-.632-1.38 0z"/>
-  <path id="tanuki-left-ear" class="tanuki-shape" fill="#e24329" d="M34 14l-9.38 9v-9l4-12.28c.205-.632 1.176-.632 1.38 0z"/>
-  <path id="tanuki-nose" class="tanuki-shape" fill="#e24329" d="M18,34.38 3,14 33,14 Z"/>
-  <path id="tanuki-right-eye" class="tanuki-shape" fill="#fc6d26" d="M18,34.38 11.38,14 2,14 6,25Z"/>
-  <path id="tanuki-left-eye" class="tanuki-shape" fill="#fc6d26" d="M18,34.38 24.62,14 34,14 30,25Z"/>
-  <path id="tanuki-right-cheek" class="tanuki-shape" fill="#fca326" d="M2 14L.1 20.16c-.18.565 0 1.2.5 1.56l17.42 12.66z"/>
-  <path id="tanuki-left-cheek" class="tanuki-shape" fill="#fca326" d="M34 14l1.9 6.16c.18.565 0 1.2-.5 1.56L18 34.38z"/>
-</svg>
-
-</div>
-</div>
-</header>
+devY = numpy.loadtxt("data/target_dev.RT")
+A = numpy.loadtxt("data/features_dev.sparseX", ndmin=2)
+I = A[:, 0]
+J = A[:, 1]
+data = A[:, 2]
+devX = coo_matrix((data, (I, J)), shape=(int(max(I))+1, feature_count))
 
 
+print (devX.shape)
 
-<div class='container navless-container'>
-<div class='content'>
-<div class='flash-container'>
-<div class='flash-alert'>
-You need to sign in or sign up before continuing.
-</div>
-</div>
+print("Training model")
 
-<div class='row'>
-<div class='col-sm-5 pull-right'>
-<div>
-<div class='login-box'>
-<div class='login-heading'>
-<h3>Sign in</h3>
-</div>
-<div class='login-body'>
-<ul class='nav-links'>
-<li class='active'>
-<a data-toggle="tab" href="#tab-ldapmain">CSCI</a>
-</li>
-<li>
-<a data-toggle="tab" href="#tab-signin">Standard</a>
-</li>
-</ul>
-<div class='tab-content'>
-<div class='active tab-pane' id='tab-ldapmain'>
-<form id="new_ldap_user" action="/users/auth/ldapmain/callback" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="6XSRJBSs6xEy8cS4pdq9m5S/fGABZFzImrJuYQ6E6znGPbkUWmkv9LsuC8xf3aNsNLNV6O0J7UzlTTOBOlKgjQ==" />
-<input type="text" name="username" id="username" class="form-control top" placeholder="CSCI Login" autofocus="autofocus" />
-<input type="password" name="password" id="password" class="form-control bottom" placeholder="Password" />
-<div class='remember-me checkbox'>
-<label for='remember_me'>
-<input type="checkbox" name="remember_me" id="remember_me" value="1" />
-<span>Remember me</span>
-</label>
-</div>
-<button name="button" type="submit" class="btn-save btn">Sign in</button>
-</form>
+outputfile = 'trainingResults_decision.csv'
+
+# write new file if it doesn't exist
+if not os.path.isfile(outputfile):
+    output = open(outputfile, 'w')
+    output.write("criterion, splitter, max_features, max_depth, min_samples_split, max_leaf_nodes, min_samples_leaf, avg_prec, prec[0], prec[1], prec[2], avg_recall, recall[0], recall[1], recall[2]\n")
+
+    output.close()
+
+def runModel(hyperparams):
+    #print(hyperparams)
+    criterion = hyperparams['criterion']
+    splitter = hyperparams['splitter']
+    max_features = hyperparams['max_features']
+    max_depth = hyperparams['max_depth']
+    min_samples_split = hyperparams['min_samples_split']
+    max_leaf_nodes = hyperparams['max_leaf_nodes']
+    min_samples_leaf = hyperparams['min_samples_leaf']            
+    
+    model = tree.DecisionTreeClassifier(criterion=criterion,
+                                        splitter=splitter,
+                                        max_features=max_features,
+                                        max_depth=max_depth,
+                                        min_samples_split=min_samples_split,
+                                        max_leaf_nodes=max_leaf_nodes,
+                                        min_samples_leaf=min_samples_leaf)
+    model.fit(X, Y)
+    
+
+    prediction = model.predict(X)
+    trainMSE = mean_squared_error(Y, prediction)
+    print( "Train Error: %.3f" % trainMSE)
+    
+    prediction = model.predict(devX)
+    devMSE = mean_squared_error(devY, prediction)
+    devMAE = median_absolute_error(devY, prediction)
+    score = model.score(devX, devY)
+    acc = accuracy_score(devY, prediction)
+    prec = precision_score(devY, prediction, average=None)
+    recall = recall_score(devY, prediction, average=None)
+    avg_prec = (sum(prec) / len(prec))
+    avg_recall = (sum(recall) / len(recall))
+    print( "Accuracy : %.5f" % acc)
+    print( "Precision : %.4f" % avg_prec)
+    print(prec)
+    print( "Recall : %.4f" % avg_recall)
+    print(recall)
+    
+    result = '{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14}\n'
+    result = result.format(criterion, splitter,
+                           max_features, max_depth, min_samples_split,
+                           max_leaf_nodes, min_samples_leaf,                             
+                           avg_prec, prec[0], prec[1], prec[2],
+                           avg_recall, recall[0], recall[1], recall[2])
+    
+    
+    output = open(outputfile, 'a')
+    output.write(result)
+    output.close()
 
 
-</div>
-<div class='tab-pane' id='tab-signin'>
-<form class="new_user" id="new_user" action="/users/sign_in" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="jquLnhmZlNG5SSJH6dhWwitatMNZEmhvay1+1w22pEyh4qOuV1xQNDCW7TMT30g1i1adS7V/2esU0iM3OWDv+A==" /><input class="form-control top" placeholder="Username or Email" autofocus="autofocus" autocapitalize="off" autocorrect="off" type="text" name="user[login]" id="user_login" />
-<input class="form-control bottom" placeholder="Password" type="password" name="user[password]" id="user_password" />
-<div class='remember-me checkbox'>
-<label for='user_remember_me'>
-<input name="user[remember_me]" type="hidden" value="0" /><input type="checkbox" value="1" name="user[remember_me]" id="user_remember_me" />
-<span>Remember me</span>
-</label>
-<div class='pull-right'>
-<a href="/users/password/new">Forgot your password?</a>
-</div>
-</div>
-<div>
-<input type="submit" name="commit" value="Sign in" class="btn btn-save" />
-</div>
-</form>
+    # Export Tree to generate an image
+    # 
+    #with open("data/decModel.dot", 'w') as f:
+    #    with open("data/features.KEY", "r") as f2:
+    #        feature_labels = f2.read().splitlines()
+    #        class_labels = ("NotName", "PersonalName", "GeoName")
+    #        f = tree.export_graphviz(model, out_file=f, feature_names=feature_labels, class_names=class_labels)
+            
+    #A = numpy.loadtxt("data/features_test.sparseX", ndmin=2)
+    #I = A[:, 0]
+    #J = A[:, 1]
+    #data = A[:, 2]
+    #testX = coo_matrix((data, (I, J)))
 
-</div>
-</div>
-</div>
-</div>
+    #prediction = model.predict(testX)
+    #numpy.savetxt("predictions.txt", prediction, fmt='%.5f')
 
-</div>
 
-</div>
-<div class='col-sm-7 brand-holder pull-left'>
-<h1>
-WWU CSCI GitLab
-</h1>
+def genHyper():
+    hyperparams = {
+        'criterion' : random.choice(['gini', 'entropy']),
+        'splitter' : random.choice(['best', 'random']),
+        'max_features' : random.choice([None, 'auto', 'sqrt', 'log2', random.randrange(100, 999)]),
+        'max_depth' : random.choice([None, random.randrange(3, 100, step=1)]),
+        'min_samples_split' : random.randrange(1, 25, step=1),
+        'max_leaf_nodes' : random.choice([None, random.randrange(50, 500, step=1)]),        
+        'min_samples_leaf' : random.choice([1, random.randrange(1, 25, step=1)])
+        }
+    return hyperparams
 
-<p>Welcome to the WWU Computer Science Department's GitLab server. Please contact <a href="https://support.cs.wwu.edu" rel="nofollow noreferrer">CS Support</a> via <a href="mailto:cs.support@wwu.edu">cs.support@wwu.edu</a> if you experience any issues using this service. We recommend familiarizing yourself with the <a href="/help">GitLab documentation</a>.</p>
+while True:
+    runModel(genHyper())
 
-<p>This GitLab instance supports <a href="https://en.wikipedia.org/wiki/Two-factor_authentication" rel="nofollow noreferrer">two-factor authentication (2FA)</a>. We encourage you to enable it if you can.</p>
-</div>
-</div>
-</div>
-</div>
-<hr>
-<div class='container'>
-<div class='footer-links'>
-<a href="/explore">Explore</a>
-<a href="/help">Help</a>
-<a href="https://about.gitlab.com/">About GitLab</a>
-</div>
-</div>
-</body>
-</html>
+
+#numpy.savetxt("predictions_random_forest.txt", prediction, fmt='%.5f')
+
+#print(A.toarray())
+
+
+
+
+
