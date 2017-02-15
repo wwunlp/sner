d3f1cd6c0a4ef9030b8397a8eddd2369c305b428
Mike Canoy <canoym@wwu.edu>
2017-02-15 12:29:28 -0800

    Fix existing tests

--- tests/test_namesfromrule.py
+++ tests/test_namesfromrule.py
@@ -1,4 +1,4 @@
-from classes import Rule, Token
+from classes import Display, Rule, Token
 from models import ner
 from scripts.ner import namesfromrule
 
@@ -11,8 +11,15 @@ def test_namesfromrule_main():
     corpus_file = 'tests/test_corpus.csv'
     seed_rules_file = 'tests/test_seed_rules.csv'
 
-    corpus = ner.tokenize_corpus(corpus_file)
-    seed_rules = ner.tokenize_seed_rules(seed_rules_file)
+    display = Display()
+
+    display.start()
+    corpus = ner.import_corpus(corpus_file, display)
+    display.finish()
+
+    display.start()
+    seed_rules = ner.import_seed_rules(seed_rules_file, display)
+    display.finish()
 
     expected = Token('aa-aa', 'bb-bb', 'cc-cc', Token.Type.personal_name)
     for rule in seed_rules:
--- tests/test_rulesperformance.py
+++ tests/test_rulesperformance.py
@@ -4,19 +4,20 @@ from scripts.ner import rulesperformance
 
 def test_rulesperformance_rateRulePerformance():
     """
-    
+
     """
 
     alpha = 0.1
     k = 2.0
-    
+    accept_threshold = 0.9
+
     rule = Rule(Rule.Type.spelling, 'bb', 1.0)
     name = Token('aa-aa', 'bb-bb', 'cc-cc', Token.Type.personal_name)
     name.name_probability = 1.0
     names = set()
     names.add(name)
 
-    rulesperformance.rateRulePerformance(names, rule, alpha, k)
+    rulesperformance.rateRulePerformance(names, rule, alpha, k, accept_threshold)
 
     expected_strength = (1.0 + alpha) / (1.0 + k * alpha)
     expected_occurrences = 1.0
