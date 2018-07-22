f7a4d42e4a4ef16934c37726f10ca2db668eeede
Mike Canoy <mike@mikecanoy.net>
2018-07-22 05:09:08 -0700

    Initial README.rst

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'SNER'
+author = 'WWUNLP SNER Team'
+copyright = '2017, ' + author
+
+release = '0.1.0'
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+pygments_style = 'sphinx'
+html_theme = 'alabaster'
+html_theme_options = {
+    'description': 'Sumerian Named Entity Recognition'
+}
---
+++ docs/index.rst
@@ -0,0 +1,14 @@
+.. include:: ../README.rst
+
+.. toctree::
+   :maxdepth: 1
+
+   changelog
+   contributing
+   contributors
+
+Indices and tables
+------------------
+- :ref:`genindex`
+- :ref:`modindex`
+- :ref:`search`
